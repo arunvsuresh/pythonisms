1a66747d4111393b52f50e020c84d80e75f3b156
Arun Suresh <arunvsuresh@gmail.com>
2020-11-28 20:46:18 -0800

    Delete decorators.py

--- decorators.py
+++
@@ -1,9 +0,0 @@
-# first class objects
-def func1(name):
-    return f"hello {name}"
-
-def func2(name):
-    return f"{name}, you're so awesome"
-
-def func_calling_func_as_first_class_obj(first_class_obj_func):
-    return first_class_obj_func('bob')
