5271a7a734a2fdeb7847ada81332be8e99cbf29a
Arun Suresh <arunvsuresh@gmail.com>
2020-11-29 14:36:34 -0800

    show how functions can be passed in data structures like lists

--- first_class_objects.py
+++ first_class_objects.py
@@ -5,12 +5,28 @@ def func2(name):
     return f"{name}, you're so awesome"
 
 # passing in functions to other functions
-def func_calling_func_as_first_class_obj(first_class_obj_func):
-    return first_class_obj_func('arun')
+def func_calling_func_as_first_class_obj(*args):
+    for func in args:
+        print(func('arun'))
 
-def greet(text):
-    return text.upper() + "!"
+    # result of passing func1 & func2 into func_calling_func_as_first_class_obj:
+    # func_calling_func_as_first_class_obj(func1, func2)
+    # output:
+    # hello arun
+    # arun, you're so awesome
 
-def func_acting_as_variable(func):
-    var = func
-    return var('text')
+# assigning func to variable to be used later on
+def func_acting_as_variable(text=None):
+    def greet(text):
+        return text.upper() + "!"
+
+    var = greet
+    if text is None:
+        return var('text')
+    else:
+        return var(text)
+
+# passing in functions to a list
+def func_in_data_structures(*args):
+    lst = [arg for arg in args]
+    return lst
