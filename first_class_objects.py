ff8463ea3688287047f128e38e4c1cfd3029dd97
Arun Suresh <arunvsuresh@gmail.com>
2020-11-29 14:06:07 -0800

    show how python functions can act as variables

--- first_class_objects.py
+++ first_class_objects.py
@@ -1,10 +1,16 @@
-# first class objects
 def func1(name):
     return f"hello {name}"
 
 def func2(name):
     return f"{name}, you're so awesome"
 
+# passing in functions to other functions
 def func_calling_func_as_first_class_obj(first_class_obj_func):
-    return first_class_obj_func('bob')
+    return first_class_obj_func('arun')
 
+def greet(text):
+    return text.upper() + "!"
+
+def func_acting_as_variable(func):
+    var = func
+    return var('text')
