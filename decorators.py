4ee5a9770fb0ea3802731fd0186a64238521b729
Arun Suresh <arunvsuresh@gmail.com>
2020-11-30 16:57:05 -0800

    exploring decorator syntactic sugar

--- decorators.py
+++ decorators.py
@@ -12,3 +12,18 @@ def hello():
 
 
 hello = my_decorator(hello)
+hello()
+
+# adding some syntactic sugar
+def syntactic_sugar_decorator(func):
+    def wrapper():
+        print('before')
+        func()
+        print('after')
+    return wrapper
+
+@syntactic_sugar_decorator
+def hi():
+    print('hi')
+
+hi()
